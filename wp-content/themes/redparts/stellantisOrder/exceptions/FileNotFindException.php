78b52acc76a9a00807d93785dda01aaa78a0edb9
cyrille <aviateur22@hotmail.fr>
2023-01-16 17:14:22 +0100

    corver ftp transfert

--- wp-content/themes/redparts/stellantisOrder/exceptions/FileNotFindException.php
+++ wp-content/themes/redparts/stellantisOrder/exceptions/FileNotFindException.php
@@ -5,9 +5,14 @@
 class FileNotFindException extends Exception {
   function __construct(string $message = '', int $code = 0 , \Throwable $previous = null)
   {
-    parent::__construct($message, $code , $previous);
-    $this->message = 'Fichier inconnu';
+    parent::__construct($message, $code , $previous);    
     $this->code = 500;
-    
+    $this->setExceptionMessage();
+  }
+
+  function setExceptionMessage() {
+    if(empty($this->message)) {
+      $this->message = 'Fichier inconnu';
+    }
   }
 }
